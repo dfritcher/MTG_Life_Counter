cfdadaf1facec3ecdf08ac5f0b6f728b4d2a9e8d
David Fritcher <dfritcher@gmail.com>
2017-04-05 12:40:45 -0400

    Adding test slider
    Added a slider control, a text control, and an empty game object called
    Slider Handler that will hold the script.

---
+++ MTG_Life_Counter/Assets/Scripts/SliderValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour {
+
+    public Text SliderText;
+    public Slider slider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSliderChanged()
+    {
+        SliderText.text = string.Format("Slider Value: {0}", slider.value);
+    }
+}
