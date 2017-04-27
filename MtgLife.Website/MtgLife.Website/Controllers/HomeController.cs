125b0d6de5f2dccf3b798fb8959163b1977a1372
Chris Smith <csmith18119@gmail.com>
2017-04-26 22:57:27 -0400

    Added Join game section

--- MtgLife.Website/MtgLife.Website/Controllers/HomeController.cs
+++ MtgLife.Website/MtgLife.Website/Controllers/HomeController.cs
@@ -13,25 +13,38 @@ namespace MtgLife.Website.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(GameViewModel viewModel) {
-            var game = CreateNewGame(viewModel);
-            var player = CreatePlayer(viewModel, game);
-
-            return Redirect("/Game/Show/" + player.PlayerId);
+        public ActionResult Index(GameViewModel viewModel, string submitButton)
+        {
+            switch (submitButton)
+            {
+                case "Create game":
+                    var game = CreateNewGame(viewModel);
+                    var playerCreated = CreatePlayer(viewModel, game.GameId);
+                    return Redirect("/Game/Show/" + playerCreated.PlayerId);
+                case "Join game":
+                    var playerJoined = CreatePlayer(viewModel, viewModel.GameId);
+                    return Redirect("/Game/Show/" + playerJoined.PlayerId);
+                default:
+                    return (View());
+            }
         }
 
-        private static CreateNewGameResponse CreateNewGame(GameViewModel viewModel) {
+        private static CreateNewGameResponse CreateNewGame(GameViewModel viewModel)
+        {
             var createNewGame = new CreateNewGameInteractor();
-            var request = new CreateNewGameRequest {
+            var request = new CreateNewGameRequest
+            {
                 StartingLifeTotal = viewModel.StartingLifeTotal,
             };
             var response = createNewGame.Invoke(request);
             return response;
         }
-        private static CreateNewPlayerResponse CreatePlayer(GameViewModel viewModel, CreateNewGameResponse game) {
+        private static CreateNewPlayerResponse CreatePlayer(GameViewModel viewModel, string gameId)
+        {
             var createNewPlayer = new CreateNewPlayerInteractor();
-            var request = new CreateNewPlayerRequest {
-                GameId = game.GameId,
+            var request = new CreateNewPlayerRequest
+            {
+                GameId = gameId,
                 PlayerName = viewModel.PlayerName,
                 LifeTotal = viewModel.StartingLifeTotal
             };
